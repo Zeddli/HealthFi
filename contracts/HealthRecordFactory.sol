ffba78e6dd38182d29a074f3522345ac0d6ccd60
Zhijian <ruj8320@gmail.com>
2025-03-05 20:09:15 +0800

    health Record Factory

---
+++ contracts/HealthRecordFactory.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import "./single_Health_Record.sol";
+
+contract HealthRecordFactory {
+    // Array to store addresses of all deployed record contracts
+    address[] public deployedRecords;
+    
+    event RecordCreated(
+        address recordAddress, 
+        string patientNric, 
+        address submittedBy, 
+        uint256 timestamp
+    );
+    
+    function createRecord(string memory _patientNric, string memory _recordHash) public {
+        HealthRecord newRecord = new HealthRecord(_patientNric, _recordHash, msg.sender);
+        deployedRecords.push(address(newRecord));
+        emit RecordCreated(address(newRecord), _patientNric, msg.sender, newRecord.timestamp());
+    }
+    
+    function getDeployedRecords() public view returns (address[] memory) {
+        return deployedRecords;
+    }
+}
